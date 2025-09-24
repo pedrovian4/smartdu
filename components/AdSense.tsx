3612958c941189e8312bd0569d57f28f2aeb4d9d
pedro <pedrovianaasking@gmail.com>
2025-09-24 14:01:35 -0300

    fix(adds): remove use-client e use effect

--- components/AdSense.tsx
+++ components/AdSense.tsx
@@ -1,29 +1,12 @@
-"use client";
-import { useEffect } from "react";
-import { GoogleAdSense, ResponsiveAdUnit } from "nextjs-google-adsense";
+import { GoogleAdSense } from "nextjs-google-adsense";
 
 
 
 export default function AdSense() {
-  useEffect(() => {
-    try {
-      // ativa o anúncio depois que o componente monta
-      // @ts-ignore
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.error("Adsense error:", err);
-    }
-  }, []);
+
 
   return (
-    <>
-      <GoogleAdSense publisherId="pub-9307683513545162" />
-
-
-
-
-
-    </>
+    <GoogleAdSense publisherId="pub-9307683513545162" />
 
   );
 }
